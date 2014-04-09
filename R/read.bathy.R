98d3059cb19ae127d554a7929a4181515fc4167c
Benoit Simon-Bouhet <besibo@gmail.com>
2014-04-09 11:51:54 +0200

    Fixed read.bathy to import non-rectangular bathy

--- R/read.bathy.R
+++ R/read.bathy.R
@@ -1,25 +1,56 @@
-read.bathy <-
-function(xyz, header=FALSE, sep=","){
+read.bathy <- function(xyz, header=FALSE, sep=","){
 
 ### xyz: three-column table with longitude (x), latitude (y) and depth (z) (no default)
 ### header: whether this table has a row of column names (default = FALSE)
 ### sep: character separating columns, (default=",")
 
-	read.table(xyz, header=header, sep=sep) -> bath	
+	bath <- read.table(xyz, header = header, sep = sep)
+	bath <- bath[order(bath[, 2], bath[, 1], decreasing = FALSE), ]
 
-	bath = bath[order(bath$V2,bath$V1, decreasing=FALSE),]
+    lat <- unique(bath[, 2]) ; bcol <- length(lat)
+    lon <- unique(bath[, 1]) ; brow <- length(lon)
 
-	unique(bath[,2]) -> lat
-	unique(bath[,1]) -> lon
+
+	# Internal function adding NA's in vector vec at position pos.missing
+	# vec: vector of data
+	# pos.missing: vector. Position of missing value in the future bathy object
+	# returns un updated vector containing both the original dat and NAs in the right place.
+	# The length of the resulting vector equals the sum of lengths of vec and pos.missing
 	
-	length(lon) -> brow
-	length(lat) -> bcol
+	add.nas <- function(vec,pos.missing) {
 
-	matrix(bath[,3], nrow=brow, ncol=bcol, byrow=FALSE, dimnames= list(lon,lat)) -> mat
-	# if(header==FALSE) names(mat) <- c("lon","lat","depth")
+		vec.new <- NULL
+		n.mis <- length(pos.missing)
+
+		for (i in 1:n.mis) {
+			modify.vec <- TRUE
+			range <- 1:(pos.missing[i]-1)
 	
-	check.bathy(mat) -> ordered.mat
-	class(ordered.mat) <- "bathy"
-	return(ordered.mat)
+			if (identical(range,1:0)) {
+				range <- 0
+				modify.vec <- FALSE
+			}
+	
+			vec.new <- c(vec.new,vec[range],NA)
+			if (modify.vec) vec <- vec[-range]
+			pos.missing <- pos.missing - pos.missing[i]
+		}
+
+		vec.new <- c(vec.new,vec)
+		return(vec.new)
+	}
+
+
+	if ((bcol*brow) == nrow(bath)) {
+		mat <- matrix(bath[, 3], nrow = brow, ncol = bcol, byrow = FALSE, dimnames = list(lon, lat))
+		} else {
+			id <- apply(bath[,-3],1,function(x) c(which(lon==x[1]) , which(lat==x[2])) )
+			pos.missing <- which(table(id[1,],id[2,])==0)
+			mat <- matrix(add.nas(bath[,3],pos.missing), nrow = brow, ncol = bcol, byrow = FALSE, dimnames = list(lon, lat))
+		}
+		
+    ordered.mat <- check.bathy(mat)
+    class(ordered.mat) <- "bathy"
+    return(ordered.mat)
 	
 }
