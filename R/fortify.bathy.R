b514dac8b267bae99639c4aeedcec7bf98b74557
Benoit Simon-Bouhet <besibo@gmail.com>
2014-04-15 22:38:08 +0200

    Removed functions depending on ggplot2
    Keep master clean for the submission of marmap 0.8
    Removed autoplot.bathy, etopo and fortify (code and help).
    A new branch dedicated to the development of functions relying on
    ggplot2 has been created.

--- R/fortify.bathy.R
+++
@@ -1,8 +0,0 @@
-fortify.bathy <- function(x, ...) {
-  # Convert an object of class bathy into a data.frame, for ggplot
-  # x   object of class bathy
-
-  x <- as.xyz(x)
-  names(x) <- c("x", "y", "z")
-  return(x)
-}
