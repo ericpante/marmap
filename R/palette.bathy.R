acc81034366e501ce6f32fe77c1c6e0b72149dba
ericpante <pante.eric@gmail.com>
2014-09-26 13:02:51 +0200

    rounding issue in palette.bathy
    corrects bug presented by Robert Herman on July 30, 2014. Problem due
    to rounding of the bathymetry data ; added round() around MAT.AMP

--- R/palette.bathy.R
+++ R/palette.bathy.R
@@ -6,21 +6,21 @@ palette.bathy <- function(mat, layers, land=FALSE, default.col="white"){
 
 	if(land == FALSE) {
 		MAT.AMP <- abs(min(mat))  # matrix amplitude
-		vec <- rep(default.col, MAT.AMP + 2) # +2 to account for depth zero and matrix start
+		vec <- rep(default.col, round(MAT.AMP,0) + 2) # +2 to account for depth zero and matrix start
 	
 		for(i in 1:length(deep)){
-			index1 <- 1 + MAT.AMP - abs(deep[i])
-			index2 <- 1 + MAT.AMP - abs(shallow[i])
+			index1 <- 1 + round(1 + MAT.AMP - abs(deep[i]), 0)
+			index2 <- 1 + round(1 + MAT.AMP - abs(shallow[i]), 0)
 			pcol <- colorRampPalette(palcol[[i]])
 			vec[index1:index2] <- pcol(length(index1:index2))
 		}
 		} else {
 		MAT.AMP <- abs(min(mat)) + abs(max(mat))  # diff(range(mat)) -> MAT.AMP
-		vec <- rep(default.col,MAT.AMP)
+		vec <- rep(default.col, MAT.AMP)
 		
 		for(i in 1:length(deep)){
-			if(deep[i] <= 0)    index1 <- 1 + abs(min(mat)) - abs(deep[i])    else index1 <- 1 + abs(min(mat)) + abs(deep[i])
-			if(shallow[i] <= 0) index2 <- 1 + abs(min(mat)) - abs(shallow[i]) else index2 <- 1 + abs(min(mat)) + abs(shallow[i])
+			if(deep[i] <= 0)    index1 <- round(1 + abs(min(mat)) - abs(deep[i]), 0)    else index1 <- round(1 + abs(min(mat)) + abs(deep[i]), 0)
+			if(shallow[i] <= 0) index2 <- round(1 + abs(min(mat)) - abs(shallow[i]), 0) else index2 <- round(1 + abs(min(mat)) + abs(shallow[i]), 0)
 			pcol <- colorRampPalette(palcol[[i]])
 			vec[index1:index2] <- pcol(length(index1:index2))
 		}
