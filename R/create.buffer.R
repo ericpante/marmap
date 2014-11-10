de66a1e8e49555082043fbc97edf043b139be82d
besibo <besibo>
2014-11-10 18:00:49 +0100

    Bug fixes for 'buffer' functions

--- R/create.buffer.R
+++ R/create.buffer.R
@@ -10,10 +10,10 @@ create.buffer <- function(mat, loc, radius){
 	map <- sp::SpatialPixelsDataFrame(points = xyz[,1:2], data = xyz, tolerance = 0.006)
     loc <- sp::SpatialPointsDataFrame(loc, data = loc)
 	
-	assign(".adeoptions",list(epsilon=0.001),envir=.adehabitatMAEnv)
+	adehabitatMA::adeoptions(epsilon=0.001)
 	temp <- adehabitatMA::buffer(loc, map, radius)
-	assign(".adeoptions",list(epsilon=0.00000001),envir=.adehabitatMAEnv)
-	
+	adehabitatMA::adeoptions(epsilon=0.00000001)
+		
 	temp <- -as.bathy(as(temp,'SpatialGridDataFrame'))
 	
 	mat[temp==0] <- NA
