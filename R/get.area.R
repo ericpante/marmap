179227982e2f11559a11d67b0379b983d120c142
bsimonbo <besibo>
2017-05-23 11:04:48 +0200

    Bug fix in get.area and improved error message in getNOAA.bathy
    get.area() has been modified to account for the changes in the
    areaPolygon() function from package geosphere
    getNOAA.bathy() now performs sanity checks for the longitude, latitude
    and resolution parameters with appropriate error messages

--- R/get.area.R
+++ R/get.area.R
@@ -1,11 +1,11 @@
 get.area <- function(mat, level.inf, level.sup=0, xlim=NULL, ylim=NULL) {
-	
+
 	# require(geosphere)
-	
+
 	if (!is(mat,"bathy") & !is(mat,"buffer")) stop("mat must be of class 'bathy' or 'buffer'")
-	
+
 	if (is(mat,"buffer")) mat <- mat[[1]]
-	
+
 	lon <- as.numeric(rownames(mat))
 	lat <- as.numeric(colnames(mat))
 
@@ -21,15 +21,15 @@ get.area <- function(mat, level.inf, level.sup=0, xlim=NULL, ylim=NULL) {
 	mat <- mat[x1b:x2b, y1b:y2b]
 	lon <- as.numeric(rownames(mat))
 	lat <- as.numeric(colnames(mat))
-	
+
 	cell.width <- (lon[2] - lon[1])
 	cell.height <- (lat[2] - lat[1])
-	
+
 	bathy2 <- ifelse(mat >= level.inf & mat <= level.sup, 1, 0)
 	cells <- apply(bathy2,2,sum)
 	cells <- cells[cells!=0]
 	c.lat <- as.numeric(names(cells))
-	
+
 	poly <- list()
 	for (i in 1:length(cells))
 		poly[[i]] <- rbind(	c(0,c.lat[i]),
@@ -37,8 +37,8 @@ get.area <- function(mat, level.inf, level.sup=0, xlim=NULL, ylim=NULL) {
 							c(cells[i]*cell.width,c.lat[i]+cell.height),
 							c(cells[i]*cell.width,c.lat[i])
 							)
-	
-	surf <- sum(sapply(poly,geosphere::areaPolygon,r=6371))
-	return <- list(Square.Km=surf, Area=bathy2, Lon=lon, Lat=lat)
-	
+
+	surf <- sum(sapply(poly,geosphere::areaPolygon))
+	return <- list(Square.Km=surf/1000000, Area=bathy2, Lon=lon, Lat=lat)
+
 }
--- R/getNOAA.bathy.R
+++ R/getNOAA.bathy.R
@@ -1,26 +1,37 @@
 getNOAA.bathy <-
 function(lon1,lon2,lat1,lat2, resolution = 4, keep=FALSE, antimeridian=FALSE){
-	
+
+	if (lon1 == lon2) stop("The longitudinal range defined by lon1 and lon2 is incorrect")
+	if (lat1 == lat2) stop("The latitudinal range defined by lat1 and lat2 is incorrect")
+	if (resolution < 1) stop("The resolution must be equal to or greater than 1")
+		
 	x1=x2=y1=y2 = NULL
-	
+
 	if (lon1 < lon2) {lon1->x1 ; lon2->x2} else {lon1->x2 ; lon2->x1}
 	if (lat1 < lat2) {lat1->y1 ; lat2->y2} else {lat1->y2 ; lat2->y1}
-	
+
 	res = resolution * 0.016666666666666667
+
+	ncell.lon <- (x2-x1)*60/resolution
+	ncell.lat <- (y2-y1)*60/resolution
+
+	if (ncell.lon < 2) stop("It's impossible to fetch an area with less than one cell. Either increase the longitudinal range or the resolution (i.e. use a smaller res value)")
+	if (ncell.lat < 2) stop("It's impossible to fetch an area with less than one cell. Either increase the latitudinal range or the resolution (i.e. use a smaller res value)")
+	if (ncell.lon < 2 & ncell.lat < 2) stop("It's impossible to fetch an area with less than one cell. Either increase the longitudinal and longitudinal ranges or the resolution (i.e. use a smaller res value)")
 	
 	fetch <- function(x1,y1,x2,y2,res) {
         WEB.REQUEST <- paste("https://gis.ngdc.noaa.gov/cgi-bin/public/wcs/etopo1.xyz?filename=etopo1.xyz&request=getcoverage&version=1.0.0&service=wcs&coverage=etopo1&CRS=EPSG:4326&format=xyz&resx=", res, "&resy=", res, "&bbox=", x1, ",", y1, ",", x2, ",", y2, sep = "")
 		dat <- suppressWarnings(try(read.table(WEB.REQUEST),silent=TRUE))
 		return(dat)
 	}
-	
+
 	# Naming the file
 	if (antimeridian) {
 		FILE <- paste("marmap_coord_",x1,";",y1,";",x2,";",y2,"_res_",resolution,"_anti",".csv", sep="")
 	} else {
 		FILE <- paste("marmap_coord_",x1,";",y1,";",x2,";",y2,"_res_",resolution,".csv", sep="")
 	}
-	
+
 	# If file exists in the working directory, load it,
 	if(FILE %in% list.files() ) {
 		cat("File already exists ; loading \'", FILE,"\'", sep="")
@@ -29,18 +40,18 @@ function(lon1,lon2,lat1,lat2, resolution = 4, keep=FALSE, antimeridian=FALSE){
 	} else { # otherwise, fetch it on NOAA server
 
 		if (antimeridian) {
-			
+
 			l1 <- x2 ; l2 <- 180 ; l3 <- -180 ; l4 <- x1
-			
+
 			cat("Querying NOAA database ...\n")
 			cat("This may take seconds to minutes, depending on grid size\n")
 			left <- fetch(l1,y1,l2,y2,res)
 			right <- fetch(l3,y1,l4,y2,res)
-			
+
 			if (is(left,"try-error")|is(right,"try-error")) {
 				stop("The NOAA server cannot be reached\n")
 			} else {
-				cat("Building bathy matrix ...\n")	
+				cat("Building bathy matrix ...\n")
 				left <- as.bathy(left) ; left <- left[-nrow(left),]
 				right <- as.bathy(right)
 				rownames(right) <- as.numeric(rownames(right)) + 360
@@ -48,26 +59,24 @@ function(lon1,lon2,lat1,lat2, resolution = 4, keep=FALSE, antimeridian=FALSE){
 				class(bath2) <- "bathy"
 				bath <- as.xyz(bath2)
 			}
-			
+
 		} else {
-			
+
 			cat("Querying NOAA database ...\n")
 			cat("This may take seconds to minutes, depending on grid size\n")
 			bath <- fetch(x1,y1,x2,y2,res)
 			if (is(bath,"try-error")) {
 				stop("The NOAA server cannot be reached\n")
 			} else {
-				cat("Building bathy matrix ...\n")	
+				cat("Building bathy matrix ...\n")
 				bath2 <- as.bathy(bath)
 			}
 		}
-	
+
 		if (keep) {
 			write.table(bath, file=FILE, sep=",", quote=FALSE, row.names=FALSE)
 		}
-		
+
 		return(bath2)
 	}
 }
-
-
