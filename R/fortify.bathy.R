2cfae824728228e4acdf84cb0b4f09948258ffef
Benoit Simon-Bouhet <besibo@gmail.com>
2023-03-24 13:38:10 +0100

    Fixed generic functions autoplot and fortify to comply with new cran rules

--- R/fortify.bathy.R
+++ R/fortify.bathy.R
@@ -1,8 +1,8 @@
-fortify.bathy <- function(x, ...) {
+fortify.bathy <- function(model, data, ...) {
   # Convert an object of class bathy into a data.frame, for ggplot
   # x   object of class bathy
 
-  x <- as.xyz(x)
+  x <- as.xyz(model)
   names(x) <- c("x", "y", "z")
   return(x)
 }
