99367e779f297e90179fe18a4c3877f98a65de6a
Eric <pante.eric@gmail.com>
2024-11-21 14:20:18 +0100

    Optimize as.bathy.R
    Key Changes:
    - Replace is() with inherits(): inherits(x, "Class") is faster than is(x, "Class").
    - Consolidate Sequence Calculations: Avoid repeated extraction of xmin, xmax, ymin, ymax.
    - Simplified Sorting and Unique Extraction: Used order only once for sorting bath.
    - Better Dimensional Checks: Direct multiplication of lengths to validate bathy matrix dimensions.
    
    > library(microbenchmark)
    > microbenchmark(
    +     original = as.bathy(nw.atlantic),
    +     optimized = optimize_as_bathy(nw.atlantic), times=1000L
    + )
    > res = microbenchmark(
    +     original = as.bathy(nw.atlantic),
    +     optimized = optimize_as_bathy(nw.atlantic), times=1000L
    + )
    > if (requireNamespace("ggplot2")) {
    +   ggplot2::autoplot(res)
    + }
    
    Unit: milliseconds
          expr      min       lq     mean   median       uq      max neval
      original 10.19250 12.68884 14.93449 13.89332 14.68839 232.1701  1000
     optimized 10.17922 12.50740 14.67705 13.74444 14.69055 223.1792  1000
    
    => small improvement

--- R/as.bathy.R
+++ R/as.bathy.R
@@ -1,47 +1,40 @@
-as.bathy <- function(x){
-
-	if (is(x,"bathy")) stop("Object is already of class 'bathy'")
-
-	if (is(x,"SpatialGridDataFrame")) x <- raster::raster(x)
-
-	# if x is a RasterLayer do this
-	if (is(x,"RasterLayer")) {
-		lat.min <- x@extent@xmin
-		lat.max <- x@extent@xmax
-		lon.min <- x@extent@ymin
-		lon.max <- x@extent@ymax
-		
-		nlat <- x@ncols
-		nlon <- x@nrows
-		
-		lon <- seq(lon.min, lon.max, length.out = nlon)
-		lat <- seq(lat.min, lat.max, length.out = nlat)
-		
-		bathy <- t(raster::as.matrix(raster::flip(x,direction="y")))
-		colnames(bathy) <- lon
-		rownames(bathy) <- lat
-	}
-	
-	# if not, it has to be a 3-column table (xyz format)
-	if (ncol(x)==3 & !exists("bathy", inherits=FALSE)) {
-		bath <- x
-	    bath <- bath[order(bath[, 2], bath[, 1], decreasing = FALSE), ]
-
-	    lat <- unique(bath[, 2]) ; bcol <- length(lat)
-	    lon <- unique(bath[, 1]) ; brow <- length(lon)
-
-		if ((bcol*brow) == nrow(bath)) {
-			bathy <- matrix(bath[, 3], nrow = brow, ncol = bcol, byrow = FALSE, dimnames = list(lon, lat))
-			} else {
-				colnames(bath) <- paste("V",1:3,sep="")
-				bathy <- reshape2::acast(bath, V1~V2, value.var="V3")
-			}
-	}
-
-	if (!exists("bathy", inherits=FALSE)) stop("as.bathy requires a 3-column table, or an object of class RasterLayer or SpatialDataFrame")
-
-	ordered.mat <- check.bathy(bathy)
-	class(ordered.mat) <- "bathy"
-	return(ordered.mat)
-
+as.bathy <- function(x) {
+    if (inherits(x, "bathy")) 
+        stop("Object is already of class 'bathy'")
+    
+    if (inherits(x, "SpatialGridDataFrame")) 
+        x <- raster::raster(x)
+    
+    if (inherits(x, "RasterLayer")) {
+        extent <- x@extent
+        lat <- seq(extent@xmin, extent@xmax, length.out = x@ncols)
+        lon <- seq(extent@ymin, extent@ymax, length.out = x@nrows)
+        bathy <- t(raster::as.matrix(raster::flip(x, direction = "y")))
+        colnames(bathy) <- lon
+        rownames(bathy) <- lat
+    } else if (ncol(x) == 3) {
+        bath <- x[order(x[, 2], x[, 1]), ]
+        lat <- unique(bath[, 2])
+        lon <- unique(bath[, 1])
+        
+        if (length(lat) * length(lon) == nrow(bath)) {
+            bathy <- matrix(
+                bath[, 3], 
+                nrow = length(lon), 
+                ncol = length(lat), 
+                byrow = FALSE, 
+                dimnames = list(lon, lat)
+            )
+        } else {
+            colnames(bath) <- c("V1", "V2", "V3")
+            bathy <- reshape2::acast(bath, V1 ~ V2, value.var = "V3")
+        }
+    } else {
+        stop("as.bathy requires a 3-column table, or an object of class RasterLayer or SpatialDataFrame")
+    }
+    
+    # Finalize bathy object
+    bathy <- check.bathy(bathy)
+    class(bathy) <- "bathy"
+    return(bathy)
 }
